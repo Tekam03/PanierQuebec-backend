cbf28c5da5c4300f675bccd5dd032c95cf138adc
Francis Mathieu-Gosselin <tekamboi@gmail.com>
2025-06-12 01:13:26 -0400

    ADD dummy file to have direct dependency

---
+++ internal/tools/tools.go
@@ -0,0 +1,8 @@
+package tools
+
+import (
+	_ "google.golang.org/protobuf/testing/prototest" // Import for protoc-gen-go
+)
+
+// This package is intentionally left empty to ensure that the tools directory
+// is recognized as a Go package. It can be used to import necessary tools
